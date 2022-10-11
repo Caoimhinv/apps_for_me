986ae44efad342d9589a7ae3aa9ea497c71206bd
Caoimhinv <caoimhinvallely@gmail.com>
2022-10-11 12:13:58 +0100

    small edits and readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# apps_for_me
+## Tune Wizard
+
+This is simple app I made to help structure my music practice! I play traditional Irish music on the fiddle. A big part of this is that there's a massive repertoire so at the start of the first covid lockdown I decided to learn a new tune or set of tunes each day to keep myself occupied! Obviously I didn't think it would last as long so when the list of tunes got to over 100, I realised I couldn't recap on each tune every day and as a result I was beginning to forget some of them. 
+This app goes through the list of tunes 1 by 1, and also picks out 5 random sets to revise, plus the last 3 sets revised. All quite simple but helpful in an extremely niche kind of way! :)
+
+
--- practice_wizard.py
+++ practice_wizard.py
@@ -10,7 +10,7 @@ with open('/Users/caoimhinvallely/Desktop/GMIT/Programming/Programming2021/other
     status = int(f.read())
 
 # asks will we move onto the next tune in the list or keep practicing current one
-x = input("Will we move on (or stay put)? y/n :")
+x = input("WILL WE MOVE ON TO THE NEXT TUNE? y/n :")
 if x == 'y':
     # moves to the next tune
     current = status + 1
@@ -24,6 +24,7 @@ if x == 'y':
     # prints todays new tune
     todaysNewTune = str(df.iloc[current])
     print("NEW TUNE FOR TODAY:\n ", todaysNewTune, "\n")
+    print("HAPPY PRACTICE!\n")
     # updates the current tune file
     with open('/Users/caoimhinvallely/Desktop/GMIT/Programming/Programming2021/otherStuff/tune_status.txt', 'w+') as t:
        t.write(str(current))
@@ -35,3 +36,4 @@ else:
     print ("RECENTLY LEARNED TUNES TO PRACTICE:\n", recentlyLearnedTunes, "\n")
     last_learnt_tune = str(df.iloc[status-1])
     print("LAST LEARNT TUNE TO REVISE:\n ", last_learnt_tune)
+    print("HAPPY PRACTICE!\n")
